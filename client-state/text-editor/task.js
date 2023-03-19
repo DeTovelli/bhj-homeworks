00e519ea9b3c2cf7e35f74d6372b4cec74b040e5
DeTovelli <detovelli@gmail.com>
2023-03-19 03:01:40 +0300

    task 8

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,10 @@
+
+const textarea = document.getElementById("editor");
+const clearBtn = document.getElementById("clear-btn")
+
+textarea.value = localStorage.text;
+textarea.oninput = () => localStorage.text = textarea.value;
+clearBtn.onclick = () => {
+    textarea.value = "";
+    localStorage.text = textarea.value;
+}
